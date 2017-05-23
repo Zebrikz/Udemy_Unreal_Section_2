d72faa59dd63dc06ec892741d43e013a26ea452f
Johann Eckman <johannsre@gmail.com>
2017-05-22 20:16:05 -0700

    Lecture 28: Instantiating Your Class.

--- BullCowGame/main.cpp
+++ BullCowGame/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include "FBullCowGame.h"
 
 void PrintIntro();
 void PlayGame();
@@ -18,6 +19,8 @@ int main()
 
 void PlayGame()
 {
+	FCowBullGame BCGame; // instantiate a new game
+
 	// loop for the number of turns asking for guesses
 	constexpr int NUMBER_OF_TURNS = 5;
 	for (int count = 1; count <= NUMBER_OF_TURNS; ++count) {
